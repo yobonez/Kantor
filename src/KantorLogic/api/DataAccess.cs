866554b5d973dc8bc65e051ecc887dadee221caa
yobonez <59935203+yobonez@users.noreply.github.com>
2025-03-21 00:19:21 +0100

    Budowanie wariantów stringów endpointowych do API

--- src/KantorLogic/api/DataAccess.cs
+++ src/KantorLogic/api/DataAccess.cs
@@ -6,50 +6,128 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Http.Formatting;
+using System.Data;
 
-namespace KantorLogic.api
+namespace KantorLogic.api;
+
+public class DataAccess
 {
-    public class DataAccess
+    private HttpClient _httpClient;
+    private readonly string _apiUrl = "https://api.nbp.pl/api/";
+    private List<CurrencyTable> currencyTables;
+
+    private readonly string[] tableCodes = { "A", "B", "C" };
+    public DataAccess()
     {
-        private HttpClient _httpClient;
-        private readonly string _apiUrl = "https://api.nbp.pl/api/";
-        private List<CurrencyTable> currencyTables;
+        _httpClient = new HttpClient();
+        currencyTables = new List<CurrencyTable>();
+    }
+    private string BuildEndpointString(string tableCode, 
+                                       int topCount = 0,
+                                       string currencyCode = null)
+    {
+        string result = string.Empty;
+        string subPath = (tableCode == "gold") 
+                         ? "cenyzlota" 
+                         : $"exchangerates/tables/{tableCode}";
 
-        private readonly string[] tableCodes = { "A", "B", "C" };
-        public DataAccess()
+        if (currencyCode != null && tableCode != "gold")
         {
-            _httpClient = new HttpClient();
-            currencyTables = new List<CurrencyTable>();
+            if (topCount > 0) result = _apiUrl + $"{subPath}/{currencyCode}/last/{topCount}";
+            else result = _apiUrl + $"{subPath}/{currencyCode}";
         }
-        public async Task<List<CurrencyTable>> GetAllNewestCurrencyTables()
-        {
-            foreach (string tableCode in tableCodes)
-            {
-                HttpResponseMessage response = await _httpClient.GetAsync(_apiUrl + $"exchangerates/tables/{tableCode}");
-                if (response.StatusCode == HttpStatusCode.OK)
-                {
-                    List<CurrencyTable> currencyTableResponse = await response.Content.ReadAsAsync<List<CurrencyTable>>();
-                    currencyTables.Add(currencyTableResponse[0]);
-                }
-            }
+        if (topCount > 0) result = _apiUrl + $"{subPath}/last/{topCount}";
+        else result = _apiUrl + $"{subPath}";
 
-            return currencyTables;
-        }
-        public async Task<CurrencyTable> GetNewestCurrencyTable(string tableCode)
-        {
-            if (currencyTables.Count != 3)
-            {
-                return (from currencyTable in currencyTables
-                        where currencyTable.Table == tableCode
-                        select currencyTable).FirstOrDefault();
-            }
+        return result;
+    }
+    private string BuildEndpointString(string tableCode,
+                                       DateTime date,
+                                       string currencyCode = null)
+    {
+        string subPath = (tableCode == "gold")
+                         ? "cenyzlota"
+                         : $"exchangerates/tables/{tableCode}";
 
-            HttpResponseMessage response = await _httpClient.GetAsync(_apiUrl + $"exchangerates/tables/{tableCode}");
-            List<CurrencyTable> currencyTableResponse = new();
+        string result = string.Empty;
+        string dateForQuery = date.Year.ToString() + '-' +
+                              date.Month.ToString() + '-' +
+                              date.Day.ToString() 
+                              ?? "today";
+
+        result = _apiUrl + $"{subPath}/{dateForQuery}";
+
+        return result;
+    }
+    private string BuildEndpointString(string tableCode,
+                                       DateTime from, 
+                                       DateTime to,
+                                       string currencyCode = null)
+    {
+        string subPath = (tableCode == "gold")
+                         ? "cenyzlota"
+                         : $"exchangerates/tables/{tableCode}";
+        string result = string.Empty;
+        string dateStartForQuery = from.Year.ToString() + '-' +
+                                   from.Month.ToString() + '-' +
+                                   from.Day.ToString()
+                                   ?? throw new Exception("Nie podano daty");
+
+        string dateEndForQuery = to.Year.ToString() + '-' +
+                                 to.Month.ToString() + '-' +
+                                 to.Day.ToString()
+                                 ?? throw new Exception("Nie podano daty");
+
+        result = _apiUrl + $"{subPath}/{dateStartForQuery}/{dateEndForQuery}";
+
+        return result;
+    }
+    public async Task<List<CurrencyTable>> GetAllCurrencyTablesNewest()
+    {
+        foreach (string tableCode in tableCodes)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(BuildEndpointString(tableCode));
             if (response.StatusCode == HttpStatusCode.OK)
-                currencyTableResponse = await response.Content.ReadAsAsync<List<CurrencyTable>>();
-
-            return currencyTableResponse[0];
+            {
+                List<CurrencyTable> currencyTableResponse = await response.Content.ReadAsAsync<List<CurrencyTable>>();
+                currencyTables.Add(currencyTableResponse[0]);
+            }
         }
+
+        return currencyTables;
+    }
+    //public async Task<List<CurrencyTable>> GetAllCurrencyTablesByDate(DateTime date)
+    //{
+    //    string strDate = date.Year.ToString() + '-' +
+    //                     date.Month.ToString() + '-' +
+    //                     date.Day.ToString();
+
+    //    foreach (string tableCode in tableCodes)
+    //    {
+    //        HttpResponseMessage response = await _httpClient.GetAsync(_apiUrl + $"exchangerates/tables/{tableCode}");
+    //        if (response.StatusCode == HttpStatusCode.OK)
+    //        {
+    //            List<CurrencyTable> currencyTableResponse = await response.Content.ReadAsAsync<List<CurrencyTable>>();
+    //            currencyTables.Add(currencyTableResponse[0]);
+    //        }
+    //    }
+    //    return currencyTables;
+    //}
+    public async Task<CurrencyTable> GetNewestCurrencyTable(string tableCode)
+    {
+        // nie wiem, czy to sie przyda...
+        //if (currencyTables.Count == 3)
+        //{
+        //    return (from currencyTable in currencyTables
+        //            where currencyTable.Table == tableCode
+        //            select currencyTable).FirstOrDefault();
+        //}
+
+        HttpResponseMessage response = await _httpClient.GetAsync(BuildEndpointString(tableCode));
+        List<CurrencyTable> currencyTableResponse = new();
+        if (response.StatusCode == HttpStatusCode.OK)
+            currencyTableResponse = await response.Content.ReadAsAsync<List<CurrencyTable>>();
+
+        return currencyTableResponse[0];
     }
 }
